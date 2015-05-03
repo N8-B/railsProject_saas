48c25fd82e3ea0562c08e197083b1b103bb699a4
Nathan Burkiewicz <nathan.burkiewicz@gmail.com>
2015-05-03 17:00:28 +0000

    Aded users show action.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,7 +12,7 @@ class ProfilesController < ApplicationController
       flash[:success] = "Profile updated!"
       redirect_to user_path( params[:user_id] )
     else
-      render :action :new
+      render action: :new
     end
   end
   
