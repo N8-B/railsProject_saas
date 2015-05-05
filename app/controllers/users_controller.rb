c22f87ae59815f93dd80b152ede9af85392570a3
Nathan Burkiewicz <nathan.burkiewicz@gmail.com>
2015-05-05 19:35:04 +0000

    Added users index action in users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  def index
+   # @users = User.find( params[:id] )
+  end
+  
   def show
     @user = User.find( params[:id] ) 
     # @profile = 
