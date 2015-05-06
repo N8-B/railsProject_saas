d03a78c4b55b4c8f36ca80dc870bfcaf38bde93f
Nathan Burkiewicz <nathan.burkiewicz@gmail.com>
2015-05-06 15:43:05 +0000

    Improved users' index page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
-   # @users = User.find( params[:id] )
+   @users = User.all
   end
   
   def show
